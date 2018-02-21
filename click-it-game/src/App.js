ae855a68ce638ee7529d72164c1c2c19d702b538
Brea Torres <abreaw328@gmail.com>
2018-02-20 20:45:40 -0500

    initial react app setup with Wrapper & ImageCard Showing

---
+++ click-it-game/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import ImageCard from "./components/ImageCard/";
+import Wrapper from "./components/Wrapper/";
+// import Title from "./components/Title";
+import images from "./images.json";
+import "./App.css";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    images
+  };
+
+  // removeFriend = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const friends = this.state.friends.filter(friend => friend.id !== id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ friends });
+  // };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        {/* <Title>Friends List</Title> */}
+        {this.state.images.map(image => (
+          <ImageCard
+            // removeFriend={this.removeFriend}
+            id={image.id}
+            key={image.id}
+            image={image.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
+
---
+++ click-it-game/src/components/ImageCard/ImageCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ImageCard.css";
+
+const ImageCard = props => (
+  <div className="card">
+    <div className="img-container">
+      <img alt={props.id} src={props.image} />
+    </div>
+  </div>
+);
+
+export default ImageCard;
